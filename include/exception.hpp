1facc23d0739d09df5eac32d584ca8a06adf4e43
jhotlann <19623263@std.stei.itb.ac.id>
2025-04-26 14:58:09 +0700

    add exceptions

--- include/exception.hpp
+++ include/exception.hpp
@@ -5,16 +5,16 @@
 #include <string>
 
 class InputOutputException : public std::exception {
-private:
-    std::string errorMessage;
+    private:
+        std::string errorMessage;
 
-public:
-    InputOutputException() : errorMessage("Terjadi kesalahan dalam proses baca tulis"){}
-    InputOutputException(const std::string& message) : errorMessage(message) {}
+    public:
+        InputOutputException() : errorMessage("Terjadi kesalahan dalam proses baca tulis"){}
+        InputOutputException(const std::string& message) : errorMessage(message) {}
 
-    const char* what() const noexcept override {
-        return errorMessage.c_str();
-    }
+        const char* what() const noexcept override {
+            return errorMessage.c_str();
+        }
 };
 
 class InventoryEror : public std::exception {
@@ -28,7 +28,72 @@ class InventoryEror : public std::exception {
         const char* what() const noexcept override {
             return errorMessage.c_str();
         }
-    };
+};
     
+class InventoryFull : public std::exception {
+    private:
+        std::string errorMessage;
+    
+    public:
+        InventoryFull() : errorMessage("Inventory sudah penuh"){}
+        InventoryFull(const std::string& message) : errorMessage(message) {}
+    
+        const char* what() const noexcept override {
+            return errorMessage.c_str();
+        }
+};
+
+class ItemNotFound : public std::exception {
+    private:
+        std::string errorMessage;
+    
+    public:
+        ItemNotFound() : errorMessage("Item tidak ditemukan"){}
+        ItemNotFound(const std::string& message) : errorMessage(message) {}
+    
+        const char* what() const noexcept override {
+            return errorMessage.c_str();
+        }
+};
+
+class InvalidCommand : public std::exception {
+    private:
+        std::string errorMessage;
+    
+    public:
+        InvalidCommand() : errorMessage("Perintah tidak valid"){}
+        InvalidCommand(const std::string& message) : errorMessage(message) {}
+    
+        const char* what() const noexcept override {
+            return errorMessage.c_str();
+        }
+};
+
+class GoldNotEnough : public std::exception {
+    private:
+        std::string errorMessage;
+    
+    public:
+        GoldNotEnough() : errorMessage("Jumlah gold tidak cukup"){}
+        GoldNotEnough(const std::string& message) : errorMessage(message) {}
+    
+        const char* what() const noexcept override {
+            return errorMessage.c_str();
+        }
+};
+
+class InvalidValue : public std::exception {
+    private:
+        std::string errorMessage;
+    
+    public:
+        InvalidValue() : errorMessage("Nilai tidak valid"){}
+        InvalidValue(const std::string& message) : errorMessage(message) {}
+    
+        const char* what() const noexcept override {
+            return errorMessage.c_str();
+        }
+};
+
 
 #endif
