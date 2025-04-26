32f7ebfbff39e572491bf3833416dd6b5e3d5f1d
Julius <juliusarthur2005@gmail.com>
2025-04-26 17:32:21 +0700

    feat: turn based effect

---
+++ src/EffectTurnBased.cpp
@@ -0,0 +1,19 @@
+#include "../include/EffectTurnBased.hpp"
+
+EffectTurnBasedBased::EffectTurnBasedBased(const std::string& name, const std::string& description, double duration, double remainingDuration)
+    : Effect(name, description, duration, remainingDuration) {}
+
+EffectTurnBasedBased::~EffectTurnBasedBased() {}
+
+EffectTurnBasedBased::EffectTurnBasedBased(EffectTurnBasedBased& other) 
+    : Effect(other) {}
+
+EffectTurnBasedBased& EffectTurnBasedBased::operator=(EffectTurnBasedBased& other) {
+    Effect::operator=(other);
+    return *this;
+}
+
+bool EffectTurnBasedBased::isTurnBased() {
+    return true;
+}
+
