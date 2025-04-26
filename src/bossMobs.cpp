be1e2539258dd9ccfaa0af3257f02b8d25547641
jhotlann <19623263@std.stei.itb.ac.id>
2025-04-26 14:43:06 +0700

    feat: rage

--- src/bossMobs.cpp
+++ src/bossMobs.cpp
@@ -1,18 +1,24 @@
 #include "bossMobs.hpp"
 
-BossMobs::BossMobs(string name, int maxHealth, int healthRegen, int maxMana, int manaRegen, int attackDamage,  int strength, int agility, int intelligence, int expReward)
- : Mobs(name, maxHealth, healthRegen, maxMana, manaRegen, attackDamage, strength, agility, intelligence, expReward, true) {
-    this->onRage = false;
+BossMobs::BossMobs(string name, int maxHealth, int healthRegen, int maxMana, int manaRegen, int attackDamage,  int strength, int agility, int intelligence, int expReward, Mobloot& mobLoots)
+ : Mobs(name, maxHealth, healthRegen, maxMana, manaRegen, attackDamage, strength, agility, intelligence, expReward, mobLoots) {
+    this->rageUsed = false;
 }
 
 BossMobs::~BossMobs() {}
 
-bool BossMobs::isOnRage() const {
-    return onRage;
+bool BossMobs::isRageUsed() const {
+    return rageUsed;
 }
 
+void BossMobs::rage() {
+    if (!isRageUsed()) {
+        setStats(getStats().getStrength() * 1.5, getStats().getAgility() * 1.5, getStats().getIntelligence() * 1.5);
+        rageUsed = true;
+    }
+}
 
 void BossMobs::reset() {
     Mobs::reset();
-    onRage = false;
+    rageUsed = false;
 }
