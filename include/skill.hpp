d4f81e641b91d9d670e72fba7b2d1d82e63fc79a
Julius <juliusarthur2005.com>
2025-04-26 10:54:58 +0700

    refactor: float to double

--- include/skill.hpp
+++ include/skill.hpp
@@ -6,28 +6,28 @@
 class Skill{
     private:
         std::string name;
-        float manaCost;
-        float masterCost;
-        float effectChance;
+        double manaCost;
+        double masterCost;
+        double effectChance;
         std::string effecType;
     public:
-        Skill(const std::string& name, float manaCost, float masterCost, float effectChance, const std::string& effectType);
+        Skill(const std::string& name, double manaCost, double masterCost, double effectChance, const std::string& effectType);
         ~Skill();
-        Skill(Skill&& other);
-        Skill& operator=(Skill&& other) = default;
+        Skill(Skill& other);
+        Skill& operator=(Skill& other) = default;
 
 
     //Setter Getter
         std::string getName() const;
-        float getManaCost() const;
-        float getMasterCost() const;
-        float getEffectChance() const;
+        double getManaCost() const;
+        double getMasterCost() const;
+        double getEffectChance() const;
 
         std::string getEffectType() const;
         void setName(const std::string& name);
-        void setManaCost(float manaCost);
-        void setMasterCost(float masterCost);
-        void setEffectChance(float effectChance);
+        void setManaCost(double manaCost);
+        void setMasterCost(double masterCost);
+        void setEffectChance(double effectChance);
         void setEffectType(const std::string& effectType);    
 };
 
