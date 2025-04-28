07f93f7790a54f70c18f93679d7f3e02c76cf407
iqbalhaidr <13523111@std.stei.itb.ac.id>
2025-04-28 11:45:32 +0700

    feat: battle mechanism

--- include/Chamber.hpp
+++ include/Chamber.hpp
@@ -1,77 +1,143 @@
 #ifndef CHAMBER_H
 #define CHAMBER_H
 
-#define MIN_ENEMIES_LAST_CHAMBER 1
-#define MAX_ENEMIES_LAST_CHAMBER 2
+#define MIN_ENEMIES_LAST_CHAMBER 2
+#define MAX_ENEMIES_LAST_CHAMBER 4
 #define MIN_ENEMIES_CHAMBER 2
 #define MAX_ENEMIES_CHAMBER 4
 #define CHANCE_BOSS_NOT_ON_LAST_CHAMBER 0.1f
 #define BASE_STATS 100;
 
+#include <string>
 #include <vector>
 
-#include "IncludeAll.hpp"
+// #include "Char.hpp"
+#include "Randomizer.hpp"
+#include "Reward.hpp"
+#include "mobs.hpp"
+#include "basicMobs.hpp"
+#include "bossMobs.hpp"
+#include "ogre.hpp"
+#include "darkKnight.hpp"
+#include "demonLord.hpp"
+#include "lich.hpp"
+// #include "slime.hpp"
+// #include "goblin.hpp"
+// #include "skeleton.hpp"
+// #include "orc.hpp"
 
 class Chamber {
-  private:
+   private:
     int rewardExp;
     int rewardGold;
     int enemyCount;
     std::vector<Mobs *> enemies;
     bool isLastChamber;
+    int minMobLevel;
+    int maxMobLevel;
+
+    /*
+     * Rasionalisasi minMobLevel, maxMobLevel:
+     *
+     * Stats mobs bergantung pada level
+     * Batas level pada setiap rank sudah sesuai difficulty scaling
+     * Jika dungeon biasa maka masukan batas level sesuai rank
+     * Jika double dungeon cukup x2 batas level
+     * Jika special dungeon cukup increment batas level
+     *     Misal:
+     *     Chamber1 -> batas = [1, 1]
+     *     Chamber2 -> batas = [2, 2]
+     *     dan seterusnya
+     *
+     *     Sehingga pada double dungeon
+     *     setiap chamber memiliki level monster sama
+     */
 
    public:
-    // 4 Sekawan
-    // <enemyCount> di randomize sesuai bound macro
-    Chamber(int rewardExp, int rewardGold, bool isLast, float difficultyScaling);
-    Chamber(const Chamber&);
+    /*
+     * Konstruktor
+     * [rewardExp, rewardGold] randomize berdasarkan batas mob level
+     * [enemyCount] randomize berdasarkan isLastChamber dan macro
+     */
+    Chamber(bool isLast, int minMobLevel, int maxMobLevel);
+    Chamber(const Chamber &);
     // Delete Mobs *
     ~Chamber();
-    Chamber& operator=(const Chamber&);
+    Chamber &operator=(const Chamber &);
 
     // Getter
     int getRewardExp() const;
     int getRewardGold() const;
     int getEnemyCount() const;
-    // std::vector<Mobs *> getEnemies() const;
+    std::vector<Mobs *> getEnemies() const;
     int isLast() const;
+    int getMinMobLevel() const;
+    int getMaxMobLevel() const;
 
-    // Setter
-    void setRewardExp(int);
-    void setRewardGold(int);
-    void setEnemyCount(int);
-    void setLast(bool);
+    // Setter (TIDAK BOLEH MENGGANTI-GANTI NILAI ATRIBUT)
+    // void setRewardExp(int);
+    // void setRewardGold(int);
+    // void setEnemyCount(int);
+    // void setLast(bool);
 
     /*
-     * Mengisi <enemies> dengan <Mobs *> sebanyak <enemyCount>
-     * <Mobs> memiliki stats sesuai dengan <difficultyScaling>
+     * Mengisi <enemies> dengan <Mobs *> sebanyak generateEnemyCount()
+     * <Mobs> memiliki level dalam batas level mob
      * Apabila last chamber maka harus diisi BossMobs
      * Apabila bukan last chamber ada kemungkinan salah satu enemies
      * diisi BossMobs maksimal 1 sesuai dengan macro chance
+     * update nilai enemy count
      */
-    void generateEnemies(float difficultyScaling);
+    void generateEnemies();
 
     /*
-     * Mengembalikan true jika berhasil membunuh semua enemy
-     * Mengembalikan false jika kabur atau mati
-     * Sistem turn based dengan musuh sequential
-     * Mereset effect, health, mana setelah keluar battle
+     * Rumus: a * minMobLevel + b * maxMobLevel + c * range
+     * a, b, c = faktor pengali
+     * range = range level mob
      */
+    int generateRewardGold();
+
+    /*
+     * Rumus sama seperti generateRewardGold()
+     */
+    int generateRewardExp();
+
+    /*
+     * Randomize jumlah musuh bedasarkan macro
+     */
+    int generateEnemyCount();
+
+    /*
+     * Randomize basic mobs dengan level sesuai, mengembalikan BasicMobs*
+     */
+    BasicMobs* generateBasicMobs(int level);
+
+    /*
+     * Randomize boss mobs dengan level sesuai, mengembalikan BossMobs*
+     */
+    BossMobs* generateBossMobs(int level);
+
+    /*
+    * Mengembalikan true jika berhasil membunuh semua enemy
+    * Mengembalikan false jika kabur atau mati
+    * Sistem turn based dengan musuh sequential
+    * Mereset effect, health, mana setelah keluar battle
+    */
     // bool battle(Char&, Reward&);
 
     /*
-     * Format:
-     * Reward Exp: <rewardExp>
-     * Reward Gold: <rewardGold>
-     * Enemy Count: <enemyCount>
-     * Last Chamber: <Yes/No>
-     * Enemies :
-     * <seq_no>. <Basic/Boss> <name of bossmobs> <strength> <agility>
-     * <intelligence>
-     * 1. [Boss] Ogre 10 20 30
-     * 2. [Boss] Dark Knight 40 50 60
-     * 3. [Basic] Slime 10 10 10
-     */
+    * Format:
+    * Reward Exp: <rewardExp>
+    * Reward Gold: <rewardGold>
+    * Enemy Count: <enemyCount>
+    * Last Chamber: <isLast>
+    * minMobLevel: <minMobLevel>
+    * maxMobLevel: <maxMobLevel>
+    * ============================
+    * Enemy <seq_no>: 
+    * enemies[i].displayInfo();
+    * ============================
+    */
     void displayInfo();
 };
 
--- src/Chamber.cpp
+++ src/Chamber.cpp
@@ -2,15 +2,10 @@
 
 #include "../include/Chamber.hpp"
 
-// 4 Sekawan
-// <enemyCount> di randomize sesuai bound macro
-Chamber::Chamber(int rewardExp, int rewardGold, bool isLast, float difficultyScaling) : rewardExp(rewardExp), rewardGold(rewardGold), isLastChamber(isLast) {
-    if (isLastChamber) {
-        enemyCount = Randomizer::random(MIN_ENEMIES_LAST_CHAMBER, MAX_ENEMIES_LAST_CHAMBER);
-    } else {
-        enemyCount = Randomizer::random(MIN_ENEMIES_CHAMBER, MAX_ENEMIES_CHAMBER);
-    }
-    generateEnemies(difficultyScaling);
+Chamber::Chamber(bool isLast, int minMobLevel, int maxMobLevel) : isLastChamber(isLast), minMobLevel(minMobLevel), maxMobLevel(maxMobLevel) {
+    rewardExp = generateRewardExp();
+    rewardGold = generateRewardGold();
+    generateEnemies();
 }
 
 Chamber::Chamber(const Chamber& c) {
@@ -19,13 +14,13 @@ Chamber::Chamber(const Chamber& c) {
     this->enemyCount = c.enemyCount;
     this->enemies = c.enemies;
     this->isLastChamber = c.isLastChamber;
+    this->minMobLevel = c.minMobLevel;
+    this->maxMobLevel = c.maxMobLevel;
 }
 
-// Delete Mobs *
 Chamber::~Chamber() {
-    std::vector<Mobs *>::iterator i = enemies.begin();
-    for (; i != enemies.end(); i++) {
-        delete *i;
+    for (int i = 0; i < enemyCount; i++) {
+        delete enemies[i];
     }
     enemies.clear();
 }
@@ -36,131 +31,173 @@ Chamber& Chamber::operator=(const Chamber& c) {
     this->enemyCount = c.enemyCount;
     this->enemies = c.enemies;
     this->isLastChamber = c.isLastChamber;
+    this->minMobLevel = c.minMobLevel;
+    this->maxMobLevel = c.maxMobLevel;
+    return *this;
 }
 
-// Getter
 int Chamber::getRewardExp() const { return rewardExp; }
 int Chamber::getRewardGold() const { return rewardGold; }
 int Chamber::getEnemyCount() const { return enemyCount; }
 std::vector<Mobs *> Chamber::getEnemies() const { return enemies; }
 int Chamber::isLast() const { return isLastChamber; }
+int Chamber::getMinMobLevel() const { return rewardExp; }
+int Chamber::getMaxMobLevel() const { return rewardGold; }
 
-// Setter
-void Chamber::setRewardExp(int rewardExp) { this->rewardExp = rewardExp; } 
-void Chamber::setRewardGold(int rewardGold) { this->rewardGold = rewardGold; }
-void Chamber::setEnemyCount(int enemyCount) { this->enemyCount = enemyCount; }
-void Chamber::setLast(bool isLast) { this->isLastChamber = isLast; }
+// void Chamber::setRewardExp(int rewardExp) { this->rewardExp = rewardExp; } 
+// void Chamber::setRewardGold(int rewardGold) { this->rewardGold = rewardGold; }
+// void Chamber::setEnemyCount(int enemyCount) { this->enemyCount = enemyCount; }
+// void Chamber::setLast(bool isLast) { this->isLastChamber = isLast; }
 
-/*
- * Mengisi <enemies> dengan <Mobs *> sebanyak <enemyCount>
- * <Mobs> memiliki stats sesuai dengan <difficultyScaling>
- * Apabila last chamber maka harus diisi BossMobs
- * [Sementara non last chamber tidak mungkin boss]
- * Apabila bukan last chamber ada kemungkinan salah satu enemies
- * diisi BossMobs maksimal 1 sesuai dengan macro chance
- */
-void Chamber::generateEnemies(float difficultyScaling) {
-    std::vector<Mobs *>::iterator i = enemies.begin();
-    for (; i != enemies.end(); i++) {
-        delete *i;
+int Chamber::generateRewardGold() {
+    int a = 2;
+    int b = 3;
+    int c = 1;
+    return a * minMobLevel + b * maxMobLevel + c * (maxMobLevel - minMobLevel);
+}
+
+int Chamber::generateRewardExp() {
+    int a = 4;
+    int b = 6;
+    int c = 2;
+    return a * minMobLevel + b * maxMobLevel + c * (maxMobLevel - minMobLevel);
+}
+
+int Chamber::generateEnemyCount() {
+    if (isLastChamber) {
+        return Randomizer::random(MIN_ENEMIES_LAST_CHAMBER, MAX_ENEMIES_LAST_CHAMBER);
+    } else {
+        return Randomizer::random(MIN_ENEMIES_CHAMBER, MAX_ENEMIES_CHAMBER);
+    }
+}
+
+BasicMobs* Chamber::generateBasicMobs(int level) {
+    switch (Randomizer::random(1, 4)) {
+        case 1:
+            // return new Slime(level)
+        case 2:
+            // return new Goblin(level)
+        case 3:
+            // return new Skeleton(level)
+        case 4:
+            // return new Orc(level)
+        default:
+            return nullptr;
+    }
+}
+
+BossMobs* Chamber::generateBossMobs(int level) {
+    switch (Randomizer::random(1, 4)) {
+        case 1:
+            return new Ogre(level);
+        case 2:
+            return new DarkKnight(level);
+        case 3:
+            return new DemonLord(level);
+        case 4:
+            return new Lich(level);
+        default:
+            return nullptr;
+    }
+}
+
+void Chamber::generateEnemies() {
+    for (int i = 0; i < enemyCount; i++) {
+        delete enemies[i];
     }
     enemies.clear();
 
+    bool isBossSpawnedInBasicChamber = false;
+    enemyCount = generateEnemyCount();
     for (int i = 0; i < enemyCount; i++) {
-        int finalDifficulty = difficultyScaling + i * 0.05f;
         if (isLastChamber) {
-            switch (Randomizer::random(1, 4)) {
-                case 1:
-                    // enemies.push_back(new BossMob1(<finalDifficulty>))
-                    break;
-                case 2:
-                    // enemies.push_back(new BossMob2(<finalDifficulty>))
-                    break;
-                case 3:
-                    // enemies.push_back(new BossMob3(<finalDifficulty>))
-                    break;
-                case 4:
-                    // enemies.push_back(new BossMob4(<finalDifficulty>))
-                    break;
-            }
+            int level = Randomizer::random(minMobLevel, maxMobLevel);
+            enemies.push_back(generateBossMobs(level));
         } else {
-            switch (Randomizer::random(1, 4)) {
-                case 1:
-                    // enemies.push_back(new BasicMob1(<finalDifficulty>))
-                    break;
-                case 2:
-                    // enemies.push_back(new BasicMob2(<finalDifficulty>))
-                    break;
-                case 3:
-                    // enemies.push_back(new BasicMob3(<finalDifficulty>))
-                    break;
-                case 4:
-                    // enemies.push_back(new BasicMob4(<finalDifficulty>))
-                    break;
+            if (!isBossSpawnedInBasicChamber) {
+                if (Randomizer::random(1, 5) == 3) { // Asumsi chance boss spawn 20% pada chamber bukan terakhir
+                    int level = Randomizer::random(minMobLevel, maxMobLevel);
+                    enemies.push_back(generateBossMobs(level));
+                    isBossSpawnedInBasicChamber = true;
+                } else {
+                    int level = Randomizer::random(minMobLevel, maxMobLevel);
+                    enemies.push_back(generateBasicMobs(level));
+                }
+            } else {
+                int level = Randomizer::random(minMobLevel, maxMobLevel);
+                enemies.push_back(generateBasicMobs(level));
             }
         }
     }
 }
 
-/*
- * Mengembalikan true jika berhasil membunuh semua enemy
- * Mengembalikan false jika kabur atau mati
- * Sistem turn based dengan musuh sequential
- * Mereset effect, health, mana setelah keluar battle
- */
 /* 
 bool battle(Char&, Reward&) {
     for (int i = 0; i < enemyCount; i++) {
         bool isCharTurn = true;
-        while (enemies[i]->getHealth() > 0 && enemies) {
-            // Parse effect
+        while (enemies[i]->getHealth() > 0 && Char.getCurrentHealth() > 0) {
             if (isCharTurn) {
-                // Choose Option
+                Char.applyTurnbasedEffect();
+                if (Char.isStunned()) {
+                    Char.decrementTurnRemainingEffect();
+                    isCharTurn = !isCharTurn
+                    continue;
+                }
+                int opt = inputOption();
+                if (opt == attack) {
+                    Char.attack(*enemies[i]);
+                } else if (opt == skill) {
+                    Char.useSkill(*enemies[i]);
+                } else if (opt == item) {
+                    Char.useItem(*enemies[i]);
+                } else if (opt == item) {
+                    return false;
+                }
+                Char.decrementTurnRemainingEffect();
             } else {
-                // Randomize Option
+                enemies[i]->applyTurnbasedEffect();
+                if (enemies[i]->isStunned()) {
+                    enemies[i]->decrementTurnRemainingEffect();
+                    isCharTurn = !isCharTurn
+                    continue;
+                }
+                int opt = Randomizer::random(1, 2);
+                if (opt == 1) {
+                    enemies[i]->attack(Char);
+                } else if (opt == 2) {
+                    enemies[i]->useSkill(Char);
+                }
+                enemies[i]->decrementTurnRemainingEffect();
             }
-            // Update effect
-
+            isCharTurn = !isCharTurn;
+        }
+        
+        if (Char.getCurrentHealth() <= 0) {
+            return false;
+        }
+        
+        if (enemies[i]->getCurrentHealth() <= 0) {
+            Reward.addExp(enemies[i]->getExpReward())
+            Reward.addItem(enemies[i]->getLootItem())
         }
     }
-
-
+    Reward.addExp(rewardExp);
+    Reward.addGold(rewardGold);
+    Char.reset()
+    return true;
 }
-
-
-
-
-
  */
 
-/*
- * Format:
- * Reward Exp: <rewardExp>
- * Reward Gold: <rewardGold>
- * Enemy Count: <enemyCount>
- * Last Chamber: <Yes/No>
- * Enemies :
- * <seq_no>. <Basic/Boss> <name of bossmobs> <strength> <agility>
- * <intelligence>
- * 1. [Boss] Ogre 10 20 30
- * 2. [Boss] Dark Knight 40 50 60
- * 3. [Basic] Slime 10 10 10
- */
 void Chamber::displayInfo() {
     std::cout << "Reward Exp: " << rewardExp << std::endl;
     std::cout << "Reward Gold: " << rewardGold << std::endl;
-    std::cout << "Enemy Count: " << enemyCount << std::endl;\
-    std::cout << "Last Chamber: ";
-    if (isLastChamber) {
-        std::cout << "Yes" << std::endl;
-    } else {
-        std::cout << "No" << std::endl;
-    }
+    std::cout << "Enemy Count: " << enemyCount << std::endl;
+    std::cout << "Last Chamber: " << isLastChamber << std::endl;
+    std::cout << "minMobLevel: " << minMobLevel << std::endl;
+    std::cout << "maxMobLevel: " << maxMobLevel << std::endl;
     std::cout << "Enemies: " << std::endl;
     for (int i = 0; i < enemyCount; i++) {
-        std::cout << i + 1 << ". " << enemies[i]->getName() << " " 
-        << enemies[i]->getStats().getStrength() << " " 
-        << enemies[i]->getStats().getAgility() << " " 
-        << enemies[i]->getStats().getIntelligence() << std::endl;
+        std::cout << "============================" << std::endl;
+        enemies[i]->displayInfo();
     }
 }
