d6a29939b4b8fd3043aea79e9401c9b6e7649750
Julius <juliusarthur2005@gmail.com>
2025-05-01 01:40:42 +0700

    feat: skill tree and node class

---
+++ include/skillNode.hpp
@@ -0,0 +1,34 @@
+#ifndef SKILLNODE
+#define SKILLNODE
+#include "skill.hpp"
+#include "EffectDamage.hpp"
+#include "EffectDefensive.hpp"
+#include "EffectHealth.hpp"
+#include "EffectHealthRegen.hpp"
+#include "EffectManaReduc.hpp"
+#include "EffectManaRegen.hpp"
+#include "EffectPoison.hpp"
+#include "EffectTurn.hpp"
+
+class SkillNode{
+    private:
+        Skill* skill;
+        SkillNode* left;
+        SkillNode* right;
+    public:
+        SkillNode(Skill* skill);
+        SkillNode(Skill* skill, SkillNode* left, SkillNode* right);
+        ~SkillNode();
+        SkillNode(const SkillNode& other);
+        SkillNode& operator=(const SkillNode& other);
+
+        SkillNode* getLeftNode() const;
+        SkillNode* getRightNode() const;
+        Skill* getLeftSkill() const;
+        Skill* getRightSkill() const;
+        void setLeftNode(SkillNode* left);
+        void setRightNode(SkillNode* right);
+    
+};
+
+#endif
