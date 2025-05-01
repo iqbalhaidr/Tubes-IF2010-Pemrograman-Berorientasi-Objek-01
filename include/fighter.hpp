7d2fdd6732fa4712aa5dc462129bfc5ca5a2bf1d
AgungLucker <iniabi838@gmail.com>
2025-05-01 21:50:45 +0700

    fix: weapon damage on calculate damage

--- include/fighter.hpp
+++ include/fighter.hpp
@@ -11,7 +11,7 @@ class Fighter : public Character {
         void updateBasicAttributes() override;
     public:
         // ctor dtor
-        Fighter(string name, int strength = 126, int agility = 17, int intelligence = 13, int level = BASE_LEVEL, int exp = BASE_EXP, int gold = BASE_GOLD, int masteryCost = BASE_MASTERY_COST);
+        Fighter(string name, int strength = 26, int agility = 17, int intelligence = 13, int level = BASE_LEVEL, int exp = BASE_EXP, int gold = BASE_GOLD, int masteryCost = BASE_MASTERY_COST);
         ~Fighter();
 
         // setter getter
