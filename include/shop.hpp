7b72be0aa57023d44365039c9aaee6e7ce23fd24
jhotlann <19623263@std.stei.itb.ac.id>
2025-05-01 14:58:59 +0700

    fix: shop attribute

--- include/shop.hpp
+++ include/shop.hpp
@@ -14,16 +14,17 @@ namespace fs = std::filesystem;
 class Shop
 {
 private:
-    std::map<std::string, std::pair<int,int>> availableItems;
-    std::map<std::string, std::vector<Item>> groupedItems; // Configuration file for shop items
-    std::map<std::string, std::pair<int,int>> shopConfig;
-public:
-    Shop(const std::string& directory, Items& itemMap);
+    std::map<std::string, std::tuple<std::string,int,int>> availableItems;
+    Items *itemMap;
+    std::map<std::string, int> shopConfig;
+public: 
+    Shop(const std::string& directory);
     void saveShop(const std::string& directory, Items& itemMap) ;
-    void buyItem(const std::string& itemName, int quantity, Inventory& inventory);
-    void sellItem(const std::string& itemName, int quantity, Inventory& inventory);
+    std::pair<int,int> buyItem(const std::string& itemName, int quantity, Inventory& inventory);
+    int sellItem(const std::string& itemName, int quantity, Inventory& inventory);
     void restock();
     void setStock(const std::string& itemName, int stock);
+    int getCurrentStock(const std::string& itemName);
     void displayDetails(std::string itemName) const;
     void displayShop() const;
 };
