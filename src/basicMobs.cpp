89b9d20e9ff9a44de3a001c8a11cbef3ec042044
AgungLucker <iniabi838@gmail.com>
2025-05-05 16:52:17 +0700

    fix: isboss kebalik

--- src/basicMobs.cpp
+++ src/basicMobs.cpp
@@ -1,7 +1,7 @@
 #include "../include/basicMobs.hpp"
 
 BasicMobs::BasicMobs(string name, int level, int strength, int agility, int intelligence, int expReward, Mobloot& mobLoots)
- : Mobs(name, level, strength, agility, intelligence, expReward, true, mobLoots) {
+ : Mobs(name, level, strength, agility, intelligence, expReward, false, mobLoots) {
 }
 
 BasicMobs::~BasicMobs() {}
--- src/bossMobs.cpp
+++ src/bossMobs.cpp
@@ -1,7 +1,7 @@
 #include "../include/bossMobs.hpp"
 
 BossMobs::BossMobs(string name, int level, int strength, int agility, int intelligence, int expReward, Mobloot& mobLoots)
- : Mobs(name, level, strength, agility, intelligence, expReward, false, mobLoots), 
+ : Mobs(name, level, strength, agility, intelligence, expReward, true, mobLoots), 
     rageUsed(false) {
 }
 
